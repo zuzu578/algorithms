ecf4441ce2aef91122dd40820a29280a02ef5e7d
zuzu <69393030+zuzu578@users.noreply.github.com>
2022-07-05 20:31:41 +0900

    Create 단어의 개수 백준.js

---
+++ 단어의 개수 백준.js
@@ -0,0 +1,9 @@
+// https://www.acmicpc.net/problem/1152
+const test = () => {
+  const letters = 'The Curious Case of Benjamin Button';
+  const arr = letters.split(" ");
+  console.log(arr.length)
+ 
+}
+
+test();
