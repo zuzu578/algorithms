5fc35d8691944690bc8c4ccc1265bb8fd5a4c246
zuzu <69393030+zuzu578@users.noreply.github.com>
2022-07-05 21:37:41 +0900

    Update 단어공부 백준.js

--- 단어공부 백준.js
+++ 단어공부 백준.js
@@ -1,27 +1,54 @@
-
 const findDistinct = (searchChar,letters) => {
-  
+
 const text = letters;
+const resultObj = {};
 let count = 0;
 let pos = text.indexOf(searchChar); //pos는 0의 값을 가집니다.
-const maxArr = [];
 
 while (pos !== -1) {
   count++;
   pos = text.indexOf(searchChar, pos + 1); // 첫 번째 a 이후의 인덱스부터 a를 찾습니다.
 }
-console.log(searchChar,count); 
+
+  resultObj.char = searchChar;
+  resultObj.count = count;
+
+  return resultObj;
+
 }
 
-
-
-
 const solutions = () => {
-  let letters = 'Mississipi';
+  const resultArr =[];
+  let letters = 'baaa';
   letters = letters.toUpperCase();
   for(let i = 0 ; i < letters.length ; i++){
-    findDistinct(letters[i],letters)
+     const resultObj = {
+        char : findDistinct(letters[i],letters).char,
+        count : findDistinct(letters[i],letters).count
+      }
+      resultArr.push(resultObj);
   }
+
+
+  let uniqueObjArray = [
+    ...new Map(resultArr.map((item) => [item["char"], item])).values(),];
+  const maxValueOfArr = Math.max(...uniqueObjArray.map(o => o.count), 0);
+
+  const filteringArr = uniqueObjArray.filter(item=> item.count === maxValueOfArr);
+  if(filteringArr.length >=2){
+    console.log('?')
+  }else{
+    const result = filteringArr.map(item=> item.char.toUpperCase())
+    console.log(result.join(""))
+  }
+  
+  
+  
+    
+  
+
+  
+  
 }
 
 
