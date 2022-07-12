3a7dd53812024748a39a5bb19a2da6883dab438b
zuzu <69393030+zuzu578@users.noreply.github.com>
2022-07-12 11:47:28 +0900

    Update 숫자 카드 백준.js

--- 숫자 카드 백준.js
+++ 숫자 카드 백준.js
@@ -8,3 +8,16 @@ for(let i = 0 ; i < targetCard.length ; i++){
     console.log(1);
   }
 }
+
+// 혹은 set 으로 해결하면 
+
+const arr1 = new Set([6,3,2,10,-10]);
+const arr2 = new Set([10,9,-5,2,3,4,5,-10]);
+
+arr2.forEach((item)=>{
+  if(arr1.has(item)){
+    console.log(1)
+  }else{
+    console.log(0)
+  }
+})
