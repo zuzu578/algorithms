dcae77e734b56aade9886921580cfacac605460e
zuzu <69393030+zuzu578@users.noreply.github.com>
2022-07-19 17:49:38 +0900

    Create 가장많은글자 백준.js

---
+++ 가장많은글자 백준.js
@@ -0,0 +1,47 @@
+const findDistinct = (searchChar,letters) => {
+
+const text = letters;
+const resultObj = {};
+let count = 0;
+let pos = text.indexOf(searchChar); //pos는 0의 값을 가집니다.
+
+while (pos !== -1) {
+  count++;
+  pos = text.indexOf(searchChar, pos + 1); // 첫 번째 a 이후의 인덱스부터 a를 찾습니다.
+}
+
+  resultObj.char = searchChar;
+  resultObj.count = count;
+
+  return resultObj;
+
+}
+
+const solutions = () => {
+  const resultArr =[];
+  let letters = 'abcab';
+  letters = letters.toUpperCase();
+  for(let i = 0 ; i < letters.length ; i++){
+     const resultObj = {
+        char : findDistinct(letters[i],letters).char,
+        count : findDistinct(letters[i],letters).count
+      }
+      resultArr.push(resultObj);
+  }
+
+
+  let uniqueObjArray = [
+    ...new Map(resultArr.map((item) => [item["char"], item])).values(),];
+  const maxValueOfArr = Math.max(...uniqueObjArray.map(o => o.count), 0);
+
+    const filteringArr = uniqueObjArray.filter(item=> item.count === maxValueOfArr);
+    const result = filteringArr.map(item=> item.char.toLowerCase())
+    console.log(result.join(""))
+ 
+  
+  
+  
+}
+
+
+solutions();
