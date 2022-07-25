500a3b93a421778fe3d07755c1c4b6c8a5ca8c15
zuzu <69393030+zuzu578@users.noreply.github.com>
2022-07-25 22:41:25 +0900

    Update 나이순 정렬 백준.js
    나이순정렬 백준 input obj 화 코드 추가

--- 나이순 정렬 백준.js
+++ 나이순 정렬 백준.js
@@ -1,17 +1,37 @@
-const obj = [
-  {
-    age : 21,
-    name :'Jugggnkyu'
-  },
-  {
-    age : 21,
-    name :'Tohyun'
-  },
-  {
-    age : 10,
-    name :'Sunyoung'
+// const obj = [
+//   {
+//     age : 21,
+//     name :'Junkyu'
+//   },
+//   {
+//     age : 21,
+//     name :'Dohyun'
+//   },
+//   {
+//     age : 20,
+//     name :'Sunyoung'
+//   }
+// ]
+const obj = [];
+let input = `21 Junkyu 21 Dohyun 20 Sunyoung`;
+input = input.split(' ');
+
+const ageArr = [];
+const nameArr = [];
+for(let i = 0 ; i < input.length ; i++ ){
+  if(isNaN(input[i]) === false){
+    ageArr.push(input[i])
+  }else{
+    nameArr.push(input[i]);
   }
-]
+}
+
+ageArr.forEach((item,index)=>{
+  obj.push({
+    age : ageArr[index],
+    name : nameArr[index]
+  })
+})
 
 obj.sort((a,b)=>{
   if(a.age < b.age){
